268b74e51fea55add01850cc6f4758c6c795d4f7
Jarrett Widman <jarrett@24m2.com>
2015-05-07 15:42:02 -0500

    linting

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var hbs = require('hbs');
-var passport = require('passport');
-var GitHubStrategy = require('passport-github').Strategy;
-var session = require('express-session');
-var flash = require('connect-flash');
+var express = require( "express" );
+var path = require( "path" );
+var favicon = require( "serve-favicon" );
+var logger = require( "morgan" );
+var cookieParser = require( "cookie-parser" );
+var bodyParser = require( "body-parser" );
+var hbs = require( "hbs" );
+var passport = require( "passport" );
+var GitHubStrategy = require( "passport-github" ).Strategy;
+var session = require( "express-session" );
+var flash = require( "connect-flash" );
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require( "./routes/index" );
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
-app.engine('html', hbs.__express);
+app.set( "views", path.join(__dirname, "views" ));
+app.set( "view engine", "html" );
+app.engine( "html", hbs.__express);
 
-hbs.registerHelper('json', function(context) {
+hbs.registerHelper( "json", function(context) {
     return JSON.stringify(context);
 });
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+//app.use(favicon(__dirname + "/public/favicon.ico" ));
+app.use(logger( "dev" ));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('less-middleware')(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(require( "less-middleware" )(path.join(__dirname, "public" )));
+app.use(express.static(path.join(__dirname, "public" )));
 
 // session
 app.use(session({
-  secret: 'adflkjaguadfnaadfjdfkKDJDFLSHsjkfh49584309dfjdfd'
+  secret: "adflkjaguadfnaadfjdfkKDJDFLSHsjkfh49584309dfjdfd"
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -43,8 +42,7 @@ app.use(flash());
 
 
 // routing
-app.use('/', routes);
-app.use('/users', users);
+app.use( "/", routes);
 
 
 // Passport session setup.
@@ -57,10 +55,10 @@ passport.deserializeUser(function(obj, done) {
 });
 
 passport.use(new GitHubStrategy({
-    clientID: 'b65c0bedd6824ac9ae55',
-    clientSecret: 'c92321a4472ce140afcd71d3c8a9755b4cd6d851',
-    callbackURL: 'http://localhost:3000/auth/github/callback',
-    scope: ['user:email', 'repo'],
+    clientID: process.env.GITHUB_CLIENTID || require( "./local-settings.json" ).gitHub.clientId,
+    clientSecret: process.env.GITHUB_CLIENTSECRET || require( "./local-settings.json" ).gitHub.clientSecret,
+    callbackURL: process.env.GITHUB_CALLBACKURL || require( "./local-settings.json" ).gitHub.callbackUrl,
+    scope: ["user:email", "repo"],
     passReqToCallback: true
   },
   function(req, accessToken, refreshToken, profile, done) {
@@ -75,19 +73,19 @@ passport.use(new GitHubStrategy({
   }
 ));
 
-app.get('/auth/github', passport.authenticate( 'github' ));
+app.get( "/auth/github", passport.authenticate( "github" ));
 
-app.get('/auth/github/callback',
-  passport.authenticate('github', { successRedirect: '/repos', failureRedirect: '/' }));
+app.get( "/auth/github/callback",
+  passport.authenticate( "github", { successRedirect: "/repos", failureRedirect: "/" }));
 
-app.get('/logout', function(req, res){
+app.get( "/logout", function(req, res){
   req.logout();
-  res.redirect('/');
+  res.redirect( "/" );
 });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error( "Not Found" );
   err.status = 404;
   next(err);
 });
@@ -96,10 +94,10 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+if (app.get( "env" ) === "development" ) {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render( "error", {
       message: err.message,
       error: err,
       layout: "layout.html"
@@ -111,7 +109,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render( "error", {
     message: err.message,
     error: {},
     layout: "layout.html"
