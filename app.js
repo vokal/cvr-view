ba9322db93a0faf40c36a5bc3ebf9cfc83853aea
Jarrett Widman <jarrett@24m2.com>
2015-06-22 15:55:12 -0500

    bump limits

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ hbs.registerHelper( "json", function(context) {
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + "/public/favicon.ico" ));
 app.use(logger( "dev" ));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(bodyParser.urlencoded({ extended: false, limit: "5mb" }));
 app.use(cookieParser());
 app.use(require( "less-middleware" )(path.join(__dirname, "public" )));
 app.use(express.static(path.join(__dirname, "public" )));
