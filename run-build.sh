20f8665f7e782c6b5d888130bf54ff106092b3d4
Jarrett Widman <jarrett@24m2.com>
2015-12-17 11:18:33 -0600

    run on CI

---
+++ run-build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+npm run testcover
+
+RESULT=$?
+
+if ! [ $RESULT ]; then
+  GITHASH="$(git rev-parse HEAD)"
+  curl -F coverage=@coverage/lcov.info "https://cvr.vokal.io/coverage?token=$CVR_TOKEN&commit=$GITHASH&removepath=/var/cache/drone/src/github.com/vokal/cvr-view/&coveragetype=lcov"
+fi
+
+exit $RESULT
