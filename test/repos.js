44531d2ee2ff0fc54eff0adfe3cd320d9c2cfe30
Jarrett Widman <jarrett@24m2.com>
2016-01-07 16:06:50 -0600

    first tests including authed routes

--- test/repos.js
+++ test/repos.js
@@ -2,24 +2,41 @@
 
 var assert = require( "assert" );
 var request = require( "supertest" );
-var app = require( "./server" );
+var app = require( "../app" );
 
 module.exports = function ()
 {
-    var server;
+    var agent;
     before( function ( done )
     {
-        app( function ( err, res )
-        {
-             server = res;
-             done( err );
-        } );
+        agent = request.agent( app );
+        done();
     } );
 
     it( "should redirect from repos page", function ( done )
     {
-        request( server )
+        agent
             .get( "/repos" )
             .expect( 302, done );
+    } );
+
+    it( "should redirect to repos on succesful login", function ( done )
+    {
+        agent
+            .post( "/auth/github/token" )
+            .field( "token", process.env.GITHUB_TESTING_AUTH_TOKEN )
+            .expect( 302 )
+            .end( function ( err, res )
+            {
+                assert.equal( res.headers.location, "/repos" );
+                done( err );
+            } );
+    } );
+
+    it( "should load repos page", function ( done )
+    {
+        agent
+            .get( "/repos" )
+            .expect( 200, done );
     } );
 };
