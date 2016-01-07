de96cdf8191a2112609bd9b0c4fce2bd29bf1e1a
Jarrett Widman <jarrett@24m2.com>
2016-01-07 16:05:56 -0600

    consolidate err handling code

--- app.js
+++ app.js
@@ -53,32 +53,13 @@ app.use( function ( req, res, next )
     next( err );
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if ( app.get( "env" ) === "development" )
-{
-  app.use( function ( err, req, res, next )
-  {
-    res.status( err.status || 500 );
-    res.render( "error", {
-      message: err.message,
-      error: err,
-      layout: "layout.html",
-      authed: req.isAuthenticated()
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
+// error handler
 app.use( function ( err, req, res, next )
 {
   res.status( err.status || 500 );
   res.render( "error", {
     message: err.message,
-    error: {},
+    error: app.get( "env" ) === "development" ? err : {},
     layout: "layout.html",
     authed: req.isAuthenticated()
   });
