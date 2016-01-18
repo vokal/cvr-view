e81de222b02f580e859c640f879e60fe7283b05b
Jarrett Widman <jarrett@24m2.com>
2016-01-18 11:16:54 -0600

    fail cvr status on build failures

--- run-build.sh
+++ run-build.sh
@@ -7,6 +7,8 @@ RESULT=$?
 if [ $RESULT == 0 ]; then
   GITHASH="$(git rev-parse HEAD)"
   curl -F coverage=@coverage/lcov.info "https://cvr.vokal.io/coverage?token=$CVR_TOKEN&commit=$GITHASH&removepath=/var/cache/drone/src/github.com/vokal/cvr-view/&coveragetype=lcov"
+else
+  curl -X POST "https://cvr.vokal.io/coverage/abort?token=$CVR_TOKEN&commit=$GITHASH"
 fi
 
 exit $RESULT
