ec778d987c656e31a96c739f11f80ca896bfa7aa
Jarrett Widman <jarrett@24m2.com>
2016-03-07 14:27:29 -0600

    update build

--- run-build.sh
+++ run-build.sh
@@ -5,10 +5,10 @@ npm run testcover
 RESULT=$?
 
 if [ $RESULT == 0 ]; then
-  GITHASH="$(git rev-parse HEAD)"
-  curl -F coverage=@coverage/lcov.info "https://cvr.vokal.io/coverage?token=$CVR_TOKEN&commit=$GITHASH&removepath=/var/cache/drone/src/github.com/vokal/cvr-view/&coveragetype=lcov"
+  echo "publish coverage for $DRONE_COMMIT"
+  curl -F coverage=@coverage/net/lcov.info "https://cvr.vokal.io/coverage?owner=$REPO_OWNER&repo=$REPO_NAME&commit=$DRONE_COMMIT&coveragetype=lcov"
 else
-  curl -X POST "https://cvr.vokal.io/coverage/abort?token=$CVR_TOKEN&commit=$GITHASH"
+  curl -X POST "https://cvr.vokal.io/coverage/abort?owner=$REPO_OWNER&repo=$REPO_NAME&commit=$GITHASH"
 fi
 
 exit $RESULT
